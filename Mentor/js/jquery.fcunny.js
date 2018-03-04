ffb82fd8de7a3b3c89f136f2bc4b22b43112a57e
wwalid <37045555+wwalid@users.noreply.github.com>
2018-03-04 20:34:52 +0300

    a

---
+++ Mentor/js/jquery.fcunny.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	    
+		$('#d1').funnyText({
+		speed: 700,
+		borderColor: 'gray',
+		activeColor: 'red',
+		color: 'gray',
+		fontSize: '2em',
+		direction: 'both'
+		
+		});
+});
